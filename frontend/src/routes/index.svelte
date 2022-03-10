547585b0a50273dad966c7d5335786c3d595797c
Jake van Biezen <jake.vanbiezen@student.hu.nl>
2022-03-10 11:42:45 +0100

    initial commit

---
+++ frontend/src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let users = [];
+	let API_ROUTE = 'http://127.0.0.1:8000/users/';
+	const getUsers = async () => {
+		const res = await fetch(API_ROUTE);
+		const data = await res.json();
+		users = data['results'];
+	};
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={getUsers}>Get users</button>
+
+{#each users as user}
+	<p>{user.username}</p>
+{/each}
